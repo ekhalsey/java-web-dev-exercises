cd9ad49434beb46d08b824511ac1966688554921
EmmaHalsey <ekhalsey@yahoo.com>
2022-08-17 14:22:39 -0500

    Classes part 1 exercises

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,11 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.HashMap;
+
+public class Course {
+
+        String subject;
+        String professor;
+        HashMap<Student, Integer> attendance;
+
+}
--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,7 +1,15 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import java.util.HashMap;
+
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student dorothy = new Student();
+        dorothy.setName("Dorothy");
+        dorothy.setGpa(4.0);
+        dorothy.setNumberOfCredits(1);
     }
 }
+
+
